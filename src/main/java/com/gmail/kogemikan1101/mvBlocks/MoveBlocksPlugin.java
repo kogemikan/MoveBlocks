a4d3b9c616e6240138c234edc50e8ef2ef684d43
kogemikan <kogemikan1101@gmail.com>
2019-04-09 15:04:09 +0900

    みすった

---
+++ src/main/java/com/gmail/kogemikan1101/mvBlocks/MoveBlocksPlugin.java
@@ -0,0 +1,19 @@
+package com.gmail.kogemikan1101.mvBlocks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author kogemikan
+ */
+public class MoveBlocksPlugin extends JavaPlugin {
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
