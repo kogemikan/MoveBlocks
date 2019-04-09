12b31636aac64f65f0622914c0db00a4c9a5245b
kogemikan <kogemikan1101@gmail.com>
2019-04-09 15:51:50 +0900

    いらないimport文消すの忘れてた

--- src/main/java/com/gmail/kogemikan1101/mvBlocks/command/MoveBlocksCommand.java
+++ src/main/java/com/gmail/kogemikan1101/mvBlocks/command/MoveBlocksCommand.java
@@ -3,9 +3,7 @@ package com.gmail.kogemikan1101.mvBlocks.command;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
-import org.graalvm.compiler.lir.sparc.SPARCMove;
 
-import javax.swing.text.TabExpander;
 import java.util.List;
 
 public class MoveBlocksCommand implements TabExecutor {
